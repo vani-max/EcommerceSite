6edf0093b8991de5e6578a20ef52a94aef4c9687
vani <vani.vashishtha2024@nst.rishihood.edu.in>
2025-09-17 19:14:48 +0530

    navigation method changes

--- src/pages/allproducts/AllProducts.jsx
+++ src/pages/allproducts/AllProducts.jsx
@@ -6,9 +6,11 @@ import myContext from '../../context/data/myContext'
 import { useDispatch, useSelector } from 'react-redux'
 import { addToCart } from '../../redux/cartSlice'
 import { toast } from 'react-toastify'
+import { useNavigate } from "react-router-dom";
 
 function Allproducts() {
   const context = useContext(myContext)
+    const navigate = useNavigate();
   const { mode, product ,searchkey,filterType,
       filterPrice} = context
 
@@ -45,13 +47,13 @@ function Allproducts() {
                      .filter((obj) => obj.price.includes(filterPrice)).map((item, index) => {
                         const { title, price, imageUrl,id } = item;
                         return (
-                            <div onClick={()=> window.location.href = `/productinfo/${id}`}   key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
+                            <div onClick={()=> navigate(`/productinfo/${id}`)}   key={index} className="p-4 md:w-1/4  drop-shadow-lg " >
                                 <div className="h-full border-2 hover:shadow-gray-100 hover:shadow-2xl transition-shadow duration-300 ease-in-out    border-gray-200 border-opacity-60 rounded-2xl overflow-hidden" style={{ backgroundColor: mode === 'dark' ? 'rgb(46 49 55)' : '', color: mode === 'dark' ? 'white' : '', }} >
                                     <div className="flex justify-center cursor-pointer" >
                                         <img className=" rounded-2xl w-full h-80 p-2 hover:scale-110 transition-scale-110  duration-300 ease-in-out" src={imageUrl} alt="blog" />
                                     </div>
                                     <div className="p-5 border-t-2">
-                                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>Shopping Platform</h2>
+                                        <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1" style={{ color: mode === 'dark' ? 'white' : '', }}>WearIt</h2>
                                         <h1 className="title-font text-lg font-medium text-gray-900 mb-3" style={{ color: mode === 'dark' ? 'white' : '', }}>{title}</h1>
                                 
                                         <p className="leading-relaxed mb-3" style={{ color: mode === 'dark' ? 'white' : '' }}>₹{price}</p>
